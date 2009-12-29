be9ed2776b646c4c11e1376ff1064fbb4a8df4d3
Paul Jensen <paul@new-bamboo.co.uk>
2009-12-29 20:50:53 +0000

    making food

--- public/snake.js
+++ public/snake.js
@@ -2,28 +2,26 @@ function checkSupported() {
   canvas = document.getElementById('canvas');
   if (canvas.getContext){
     ctx = canvas.getContext('2d');
-    // Canvas is supported
-
-    // The current position of the Snake's head, as xy coordinates
     this.currentPosition = {'x':50, 'y':50};
-    
-    // Sets the grid dimensions as one value
     this.gridSize = 10;
-
-
-    // This sets the fill color to red
-    ctx.fillStyle = "rgb(200,0,0)";
+    snakeBody = [];
     drawSnake();
+    makeFoodItem();
     direction = 'right';
     setInterval(moveSnake,100);
   } else {
-    // Canvas is not supported
     alert("We're sorry, but your browser does not support the canvas tag. Please use any web browser other than Internet Explorer.");
   }
 }
 
 function drawSnake() {
+  snakeBody.push([currentPosition['x'], currentPosition['y']]);
+  ctx.fillStyle = "rgb(200,0,0)";
   ctx.fillRect(currentPosition['x'], currentPosition['y'], gridSize, gridSize);
+  if (snakeBody.length > 3) {
+    var itemToRemove = snakeBody.shift();
+    ctx.clearRect(itemToRemove[0], itemToRemove[1], gridSize, gridSize);
+  }
 }
 
 function leftPosition(){
@@ -42,27 +40,43 @@ function downPosition(){
   return currentPosition['y'] + gridSize;
 }
 
+function whichWayToGo(axisType){  
+  if (axisType=='x') {
+    a = (currentPosition['x'] > canvas.width / 2) ? moveLeft() : moveRight();
+  } else {
+    a = (currentPosition['y'] > canvas.height / 2) ? moveUp() : moveDown();
+  }
+}
+
 function moveUp(){
   if (upPosition() >= 0) {
     executeMove('up', 'y', upPosition());
+  } else {
+    whichWayToGo('x');
   }
 }
 
 function moveDown(){
   if (downPosition() < canvas.height) {
     executeMove('down', 'y', downPosition());    
+  } else {
+    whichWayToGo('x');
   }
 }
 
 function moveLeft(){
   if (leftPosition() >= 0) {
     executeMove('left', 'x', leftPosition());
+  } else {
+    whichWayToGo('y');
   }
 }
 
 function moveRight(){
   if (rightPosition() < canvas.width) {
     executeMove('right', 'x', rightPosition());
+  } else {
+    whichWayToGo('y');
   }
 }
 
@@ -70,6 +84,20 @@ function executeMove(dirValue, axisType, axisValue) {
   direction = dirValue;
   currentPosition[axisType] = axisValue;
   drawSnake();
+}
+
+function makeFoodItem(){
+  suggestedPoint = [Math.floor(Math.random()*(canvas.width/10))*10, Math.floor(Math.random()*(canvas.height/10))*10];
+  if (snakeBody.some(hasPoint)) {
+    makeFoodItem();
+  } else {
+    ctx.fillStyle = "rgb(10,100,0)";
+    ctx.fillRect(suggestedPoint[0], suggestedPoint[1], gridSize, gridSize);
+  };
+}
+
+function hasPoint(element, index, array) {
+  return (element[0] == suggestedPoint[0] && element[1] == suggestedPoint[1]);
 }
 
 
@@ -87,22 +115,18 @@ document.onkeydown = function(event) {
  
   switch(keyCode)
   {
-    // left 
     case 37:
       moveLeft();
       break;
      
-    // up 
     case 38:
       moveUp();
       break; 
       
-    // right 
     case 39:
       moveRight();
       break; 
     
-    // down
     case 40:
       moveDown();
       break; 
